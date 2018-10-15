a680a57405d055eb8b07003370aed235035ebd8d
Pedro Aaron <p.luis.aaron@gmail.com>
2018-10-15 11:04:09 -0500

    Window Shopper 02 - Input accessory views & IBDesignable
    Window Shopper 02 - Input accessory views & IBDesignable

--- window-shopper/window-shopper/Views/CurrencyTxtField.swift
+++ window-shopper/window-shopper/Views/CurrencyTxtField.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 
-class CurrencyTxtField: UITextField {
 
+class CurrencyTxtField: UITextField {
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        
         backgroundColor=#colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         layer.cornerRadius=5.0
         textAlignment = .center
@@ -21,7 +23,9 @@ class CurrencyTxtField: UITextField {
             let place=NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
             attributedPlaceholder=place
             textColor=#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-            
-        }
+        
     }
+        
+    }
+
 }
