a8294db76bff40b56891042a612a510ffbd2a2f4
Pedro Aaron <p.luis.aaron@gmail.com>
2018-10-20 10:59:13 -0500

    App: Coder Swag 01 - Project creation & IBOutlets
    App: Coder Swag 01 - Project creation & IBOutlets

---
+++ coder swag/coder swag/Controller/CategoriesVC.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  coder swag
+//
+//  Created by PEDRO LUIS AARON R on 10/20/18.
+//  Copyright © 2018 paaron. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController {
+    
+    @IBOutlet weak var categoryTable:UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
