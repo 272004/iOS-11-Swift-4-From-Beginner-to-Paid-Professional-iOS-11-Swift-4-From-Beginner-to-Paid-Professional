8290ba222b877376c2ff3027f1569617a9ab717e
Pedro Aaron <p.luis.aaron@gmail.com>
2018-10-22 19:31:42 -0500

    App: Coder Swag 03 - Collection views (grid layouts)
    App: Coder Swag 03 - Collection views (grid layouts)

---
+++ coder swag/coder swag/View/ProductCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCell.swift
+//  coder swag
+//
+//  Created by PEDRO LUIS AARON R on 10/22/18.
+//  Copyright © 2018 paaron. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage :UIImageView!
+    @IBOutlet weak var productTitle :UILabel!
+    @IBOutlet weak var productPrice :UILabel!
+    
+    
+}
